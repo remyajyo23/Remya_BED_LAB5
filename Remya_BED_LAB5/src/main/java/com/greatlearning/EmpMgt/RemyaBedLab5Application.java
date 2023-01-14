b50043a09a14fca3400f0f2f08e1d106813ab399
remyajyo23 <42464800+remyajyo23@users.noreply.github.com>
2023-01-14 19:57:30 +0530

    Add files via upload

---
+++ Remya_BED_LAB5/src/main/java/com/greatlearning/EmpMgt/RemyaBedLab5Application.java
@@ -0,0 +1,13 @@
+package com.greatlearning.EmpMgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemyaBedLab5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemyaBedLab5Application.class, args);
+	}
+
+}
---
+++ Remya_BED_LAB5/src/test/java/com/greatlearning/EmpMgt/RemyaBedLab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.EmpMgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemyaBedLab5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
